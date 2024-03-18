8d69e4bfc4cf9c0bdaba69dca991e1860f7732d6
b1-2-ac <149090519+Afresh2@users.noreply.github.com>
2024-03-18 15:32:34 +0900

    papplan - spring initializr

---
+++ papplan/src/main/java/com/boogle/papplan/PapplanApplication.java
@@ -0,0 +1,13 @@
+package com.boogle.papplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapplanApplication.class, args);
+	}
+
+}
