413af23102cc247d9f52f56f520858a16ace5fb9
afresh2 <afresh320@daum.net>
2024-03-21 14:05:52 +0900

    second commit

--- papplan/src/main/java/com/boogle/papplan/Dto/Task.java
+++
@@ -1,3 +0,0 @@
-import javax.persistence.Entity;
-
-@Entity
--- papplan/src/main/java/com/boogle/papplan/PapplanApplication.java
+++ papplan/src/main/java/com/boogle/papplan/PapplanApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.com.boogle.papplan;
+package com.boogle.papplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
