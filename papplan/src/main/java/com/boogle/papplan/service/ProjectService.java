3c5e5fd60b2b34bcb94af27c20f6510621ab35ec
raheego <54056684+raheego@users.noreply.github.com>
2024-03-25 09:55:42 +0900

    [ADD] 프로젝트 상세페이지

--- papplan/src/main/java/com/boogle/papplan/service/ProjectService.java
+++ papplan/src/main/java/com/boogle/papplan/service/ProjectService.java
@@ -19,8 +19,10 @@ public class ProjectService {
     private final ContributorRepository contributorRepository; //참여자
 
     @Autowired
-    public ProjectService(ProjectRepository projectRepository, ContributorRepository contributorRepository) {
+    public ProjectService(ProjectRepository projectRepository, ContributorRepository contributorRepository, ProjectRepository projectRepository1, ContributorRepository contributorRepository1) {
 
+        this.projectRepository = projectRepository1;
+        this.contributorRepository = contributorRepository1;
     }
 
     // PM으로 참여한 프로젝트를 가져오는 메서드
