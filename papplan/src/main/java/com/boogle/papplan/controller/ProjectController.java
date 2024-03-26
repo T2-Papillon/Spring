41bbbf25205879032fc79ef335750cf6c5cfc535
dana096 <carpediem096@naver.com>
2024-03-26 16:58:32 +0900

    [MOD] ProjectController/TaskController api 링크 변경 [ADD] SearchController 프론트엔드 관련 @CrossOrigin 어노테이션 추가

--- papplan/src/main/java/com/boogle/papplan/controller/ProjectController.java
+++ papplan/src/main/java/com/boogle/papplan/controller/ProjectController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/project")
 public class ProjectController {
 
     private final ProjectService projectService;
