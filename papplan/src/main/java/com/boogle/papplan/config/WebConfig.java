e59ba8bf525048e7ce8e515fdfa36b67ca4d3faf
choi3179 <jacobis@naver.com>
2024-03-27 16:40:02 +0900

    [FIX] v0.1.3 Login API Fix
    - [x] 로그인 응답으로 사용자 정보 response 추가
    - [x] CORS WebConfig 추가

---
+++ papplan/src/main/java/com/boogle/papplan/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.boogle.papplan.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173");
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
