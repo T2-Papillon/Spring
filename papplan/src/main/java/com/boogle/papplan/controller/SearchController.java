a05389246294b0ffbeb3b2dd4e22209c40c53302
Afresh2 <afresh320@daum.net>
2024-03-28 17:18:33 +0900

    @CrossOrigin추가

--- papplan/src/main/java/com/boogle/papplan/controller/SearchController.java
+++ papplan/src/main/java/com/boogle/papplan/controller/SearchController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/search")
+@CrossOrigin("http://localhost:5173/")
 public class SearchController {
 
     private final SearchService searchService;
--- papplan/src/main/java/com/boogle/papplan/controller/StatisticController.java
+++ papplan/src/main/java/com/boogle/papplan/controller/StatisticController.java
@@ -4,13 +4,11 @@ import com.boogle.papplan.dto.StatisticCombinedDTO;
 import com.boogle.papplan.service.StatisticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/statistic")
+@CrossOrigin("http://localhost:5173/")
 public class StatisticController {
     private final StatisticService statisticService;
 
