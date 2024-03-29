2d27c0c7033714df931486ced7ad539fefddf684
dana096 <carpediem096@naver.com>
2024-03-29 16:50:48 +0900

    [MOD] projectDetail 관련 api 링크 변경

--- papplan/src/main/java/com/boogle/papplan/controller/ProjectController.java
+++ papplan/src/main/java/com/boogle/papplan/controller/ProjectController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin("http://localhost:5173/")
 public class ProjectController {
 
     private final ProjectService projectService;
@@ -58,7 +59,7 @@ public class ProjectController {
     }
 
     // 프로젝트 상세정보 조회
-    @GetMapping("/{projNo}")
+    @GetMapping("/detail")
     public ResponseEntity<ProjectDTO> showProjectDetail(@RequestParam("projNo") Integer projNo) {
         ProjectDTO projectDto = projectService.getProjectByProjNo(projNo);
 
--- papplan/src/main/java/com/boogle/papplan/controller/TaskController.java
+++ papplan/src/main/java/com/boogle/papplan/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/task")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173/")
 public class TaskController {
 
     private final TaskService taskService;
