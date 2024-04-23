27bf2bb5c759653292ac2076641318413955e8de
choi3179 <jacobis@naver.com>
2024-04-23 11:30:25 +0900

    [ADD] Spring code Timezone set

--- papplan/src/main/java/com/boogle/papplan/PapplanApplication.java
+++ papplan/src/main/java/com/boogle/papplan/PapplanApplication.java
@@ -1,13 +1,21 @@
 package com.boogle.papplan;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 //@EnableSwagger2
 public class PapplanApplication {
+
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PapplanApplication.class, args);
